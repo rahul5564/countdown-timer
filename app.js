6d28b302c0ded471657101281e2f15c0cc1d263d
rahul <sraghavendra5564@gmail.com>
2020-12-17 16:46:21 +0530

    Ready to Launch

---
+++ app.js
@@ -0,0 +1,28 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const newYears= "1 jan 2030";
+
+function countdown() {
+    const newYearDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const Totalseconds = (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(Totalseconds / 3600 / 24);
+    const hours = Math.floor(Totalseconds / 3600) % 24;
+    const mins = Math.floor(Totalseconds / 60) % 60;
+    const seconds = Math.floor(Totalseconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secondsEl.innerHTML = seconds;
+
+}
+
+countdown();
+
+setInterval(countdown , 1000);
